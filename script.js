c6bd2caabd8a879c7d097976c39cc9eb66e42db1
Gabriel Acosta <gabriel.a@bidcom.com.ar>
2023-07-01 16:52:42 -0300

    javascript notas

---
+++ script.js
@@ -0,0 +1,28 @@
+// // alert ("hola")
+
+// var nombre = "Gabriel";
+
+// // Declaracion de la Variable
+
+// let apellido = "Acosta";
+
+// const curso = "Javascript";
+
+// console.log(nombre)
+// console.log(apellido)
+// console.log(curso)
+
+// typeof
+// variable -> Retorna el tipo de dato que es una variable
+// let numero1= 5;
+// console.log("Tipo: ",typeof (numero1));
+// const texto="Hola mundo!";
+// console.log("Tipo:",typeof (texto))
+// // Template literals o template strings
+// son cadenas de caracteres en JavaScript, las cuales pueden tener variables y expresiones interpoladas
+// dentro dellas para generar un string con formato dinámico. Estas son escritas
+// entre comillas dobles `"` o simples `'`, pero se diferencian por su
+// sintaxis especial.
+// En este caso usaremos comillas sencillas ya que no necesitamos usar
+// saltos de línea ni tampoco concatenación entre los valores del array. Usando ${
+//     } podemos insertar cualquier valor dentro de nuestra literal sin problema alguno.

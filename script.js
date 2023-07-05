4656811345dddbba8f5555a895c1288053b10985
Gabriel Hernan Acosta <acostagabrielhernan1@gmail.com>
2023-07-04 23:30:02 -0300

    javascript v0.01

--- script.js
+++ script.js
@@ -1,28 +1,61 @@
-// // alert ("hola")
+let nombre = prompt("Ingresa tu nombre");
 
-// var nombre = "Gabriel";
+let edad = Number(prompt("Ingresa tu edad"));
 
-// // Declaracion de la Variable
+let favorito = prompt("Ingresa tu juego favorito");
 
-// let apellido = "Acosta";
+const BLANCO = " ";
 
-// const curso = "Javascript";
+function presentacion(nombre, edad, favorito) {
 
-// console.log(nombre)
-// console.log(apellido)
-// console.log(curso)
+    alert("Tu nombre es " + nombre + " tenes " + edad + " años y tu juego favorito es " + favorito);
+    
+}
 
-// typeof
-// variable -> Retorna el tipo de dato que es una variable
-// let numero1= 5;
-// console.log("Tipo: ",typeof (numero1));
-// const texto="Hola mundo!";
-// console.log("Tipo:",typeof (texto))
-// // Template literals o template strings
-// son cadenas de caracteres en JavaScript, las cuales pueden tener variables y expresiones interpoladas
-// dentro dellas para generar un string con formato dinámico. Estas son escritas
-// entre comillas dobles `"` o simples `'`, pero se diferencian por su
-// sintaxis especial.
-// En este caso usaremos comillas sencillas ya que no necesitamos usar
-// saltos de línea ni tampoco concatenación entre los valores del array. Usando ${
-//     } podemos insertar cualquier valor dentro de nuestra literal sin problema alguno.
+presentacion(nombre, edad, favorito)
+
+console.log("¡¡Hola!!" + BLANCO + nombre);
+
+console.log("¡Vaya tienes" + BLANCO + edad + BLANCO + "de edad!");
+
+if (nombre.toLowerCase() == "eric") {
+
+    console.log("Bienvenido a Zona Gaming Tutor Eric");
+
+}
+
+else if (nombre.toLowerCase() == "gabriel") {
+
+    console.log("Bienvenido de vuelta Jefe");
+
+}
+
+else {
+
+    alert("Bienvenido a tu proxima pagina gaming favorita");
+
+}
+
+let usuario = prompt("¿Desea crear un usuario? Si o No")
+
+do {
+
+    if (usuario.toLowerCase() == "si") {
+
+        alert("Gracias por crear un usuario")
+
+    }
+
+    else if (usuario.toLowerCase() == "no") {
+
+        alert("¡¡Te esperamos cuando quieras crear uno!!")
+
+    }
+
+    else {
+
+        usuario = prompt("Por favor indique solamente si o no")
+
+    }
+
+} while (usuario.toLowerCase() !== "si" && usuario.toLowerCase() !== "no");
